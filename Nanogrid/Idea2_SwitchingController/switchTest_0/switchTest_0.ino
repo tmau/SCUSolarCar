70b513eec3e9ccb315a9a2c52b530c5900dcee90
tmau <tmau@scu.edu>
2017-07-17 13:52:49 -0700

    Testing Nanogrid Switching Controller
    Added the code to test the basic idea of the switching controller

---
+++ Nanogrid/Idea2_SwitchingController/switchTest_0/switchTest_0.ino
@@ -0,0 +1,78 @@
+#define in1 5
+#define in2 6
+#define in3 7
+
+
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  
+
+}
+
+void loop() {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  Serial.println("0 0 0");
+
+  delay(1000);
+
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, LOW);
+  Serial.println("0 1 0");
+
+
+  delay(1000);
+
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, HIGH);
+  Serial.println("0 0 1");
+
+
+  delay(1000);
+
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, HIGH);
+  Serial.println("0 1 1");
+
+  delay(1000);
+
+
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  Serial.println("1 0 0");
+
+
+  delay(1000);
+
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, HIGH);
+  Serial.println("1 0 1");
+
+  delay(1000);
+
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, LOW);
+  Serial.println("1 1 0");
+
+  delay(1000);
+
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, HIGH);
+  Serial.println("1 1 1");
+
+  delay(1000);
+
+}
