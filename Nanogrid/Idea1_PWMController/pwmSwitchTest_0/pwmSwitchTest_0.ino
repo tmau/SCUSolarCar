dfa560c232a0361b4821ae03fc12e87198a1a33a
tmau <tmau@scu.edu>
2017-07-20 15:33:18 -0700

    Added Nanogrid PWM Control

---
+++ Nanogrid/Idea1_PWMController/pwmSwitchTest_0/pwmSwitchTest_0.ino
@@ -0,0 +1,23 @@
+#define in1 7
+#define potent 3
+
+int potentRead;
+int pwmControl;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(in1,OUTPUT);
+  //pinMode(potent,INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  potentRead=analogRead(potent);
+  pwmControl=potentRead/4;
+  analogWrite(in1,pwmControl);
+  Serial.println(pwmControl );
+}
+ 
