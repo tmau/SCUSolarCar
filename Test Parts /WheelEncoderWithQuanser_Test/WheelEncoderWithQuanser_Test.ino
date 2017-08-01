7b658c91c3ed366a271aad0168d8c585166ddb05
tmau <tmau@scu.edu>
2017-08-01 16:07:20 -0700

    Added Test for Wheel Encoder
    Used the Quanser to test to see if the wheel encoder would work with
    the reader. We were able to confirm that we were seeing all the ticks.

---
+++ Test Parts /WheelEncoderWithQuanser_Test/WheelEncoderWithQuanser_Test.ino
@@ -0,0 +1,50 @@
+
+
+#define CHA 2
+#define CHB 3
+
+
+volatile int master_count = 0; // universal count
+volatile byte INTFLAG1 = 0; // interrupt status flag
+
+void setup() { 
+  pinMode(CHA, INPUT);
+  pinMode(CHB, INPUT);
+
+  
+  Serial.begin(9600); 
+  Serial.println(master_count);
+  
+  attachInterrupt(0, flag, RISING);  
+  // interrupt 0 digital pin 2 positive edge trigger
+}
+
+void loop() {
+
+//  if (INTFLAG1)   {
+//       Serial.println(master_count);
+//       delay(500);
+//     INTFLAG1 = 0; // clear flag
+//} 
+
+
+} 
+
+
+void flag() {
+  INTFLAG1 = 1;
+  // add 1 to count for CW
+  if (digitalRead(CHA) && !digitalRead(CHB)) {
+    master_count++ ;
+
+           Serial.println(master_count);
+
+  }
+  // subtract 1 from count for CCW
+  if (digitalRead(CHA) && digitalRead(CHB)) {
+    master_count-- ;
+
+           Serial.println(master_count);
+
+  } 
+}
