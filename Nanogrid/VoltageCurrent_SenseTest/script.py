af1a01b645098a5079e8415342816f15a97cd19f
tmau <tmau@scu.edu>
2017-08-17 13:56:24 -0700

    Using .csv file not .txt and added comments to the python script

--- Nanogrid/VoltageCurrent_SenseTest/script.py
+++ Nanogrid/VoltageCurrent_SenseTest/script.py
@@ -1,12 +1,22 @@
+########## TO RUN SCRIPT ##########
+#In terminal, type: python script.py
+
 ##############
 ## Script listens to serial port and writes contents into a file
 ##############
 ## requires pySerial to be installed 
 import serial
 
-serial_port = '/dev/cu.usbmodem1411';
+#Change the serial_port value if not an Arduino Mega 
+#Go to your arduino code and in the main menu, go to  
+#Tools then port and choose the path of the nonbluetooth port
+serial_port = '/dev/cu.usbmodem1411'; 
+
 baud_rate = 9600; #In arduino, Serial.begin(baud_rate)
-write_to_file_path = "/Users/taylorpiggy/Desktop/SCUSolarCar/Nanogrid/VoltageCurrent_SenseTest/output.txt";
+
+#We can write directly to the .csv file  
+#Change the path to where you want your computer to write the output to
+write_to_file_path = "/Users/taylorpiggy/Desktop/SCUSolarCar/Nanogrid/VoltageCurrent_SenseTest/output.csv";
 
 output_file = open(write_to_file_path, "w+");
 ser = serial.Serial(serial_port, baud_rate)
