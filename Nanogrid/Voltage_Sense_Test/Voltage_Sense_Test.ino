522a5b44c8c438d4ab56f664f4d7ae3f9acc4148
tmau <tmau@scu.edu>
2017-08-17 13:49:12 -0700

    Added Voltage test
    It will test the voltage from 4 analog pins and output them to a
    textfile which you then change the extension to a .csv file which
    allows you to open the file in excel and create graphs

--- Nanogrid/Voltage_Sense_Test/Voltage_Sense_Test.ino
+++ Nanogrid/Voltage_Sense_Test/Voltage_Sense_Test.ino
@@ -5,7 +5,18 @@ void setup() {
 
 void loop() {
 //  double res = analogRead(A2)*0.1536919479/5;
- float res = analogRead(A2)*0.02797202797; //multiply by 24/858
-  Serial.println(res);
+ float res1 = analogRead(A2)*0.02797202797; //multiply by 24/858
+ float res2 = analogRead(A3)*0.02797202797;
+ float res3 = analogRead(A4)*0.02797202797;
+ float res4 = analogRead(A5)*0.02797202797;
+ 
+//  Serial.println(res);
+  Serial.print(res1, DEC);             // send the first value   
+  Serial.print(",");                   // separated by a comma
+  Serial.print(res2, DEC);             // other values get sent as above  
+  Serial.print(","); 
+  Serial.print(res3, DEC);  
+  Serial.print(","); 
+  Serial.print(res4, DEC);     
+  Serial.println();                    // print a linefeed character     
 }
-
