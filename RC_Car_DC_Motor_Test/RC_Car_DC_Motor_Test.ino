3d37658e8c1098fc7ea6369a46e82c7e29a9d566
tmau <tmau@scu.edu>
2017-08-22 14:37:19 -0700

    RC Car motor test is complete

--- RC_Car_DC_Motor_Test/RC_Car_DC_Motor_Test.ino
+++ RC_Car_DC_Motor_Test/RC_Car_DC_Motor_Test.ino
@@ -3,7 +3,11 @@
 int enA = 10;
 int in1 = 9;
 int in2 = 8;
+int in3 = 7;
+int in4 = 6;
+int enB = 5;
 int Speed = 200; 
+int SpeedB = 255; 
 
 void Forward();
 void Reverse(); 
@@ -14,14 +18,33 @@ void setup()
   pinMode(enA, OUTPUT);
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
+  pinMode(enB, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
 }
 
 void loop()
 {
+  Left(); 
+  delay(2000);
+  Right(); 
+  delay(2000);
   Forward(); 
   delay(2000);
   Backward();
   delay(2000);
+}
+void Left(){
+  //Left
+  analogWrite(enB, SpeedB);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);  
+}
+void Right(){
+  //Right
+  analogWrite(enB, SpeedB);
+  digitalWrite(in4, LOW);  
+  digitalWrite(in3, HIGH);
 }
 void Forward()
 {
@@ -34,4 +57,13 @@ void Backward()
   analogWrite(enA, Speed);
   digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);
+}
+void Brake()
+{
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+  analogWrite(enA, Speed);
+  analogWrite(enB, Speed);
 }
