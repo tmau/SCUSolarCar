a81f3e4a56caa2b90789486041475d8720785413
tmau <tmau@scu.edu>
2017-08-22 19:49:38 -0700

    RC Car with Ping Sensor
    Added the Ping Ultrasonic Sensor to the RC Car and was able to detect
    if an obstacle was going to be hit.  If the obstacle was less than a
    foot away, the robot would stop.

---
+++ RC_Car_PingSensor_DC_Motor_Test/RC_Car_PingSensor_DC_Motor_Test.ino
@@ -0,0 +1,138 @@
+//Ping Sensor Pin 
+const int pingPin = 4;
+
+// connect motor controller pins to Arduino digital pins
+// motor one
+int enA = 10;
+int in1 = 9;
+int in2 = 8;
+//motor two 
+int in3 = 7;
+int in4 = 6;
+int enB = 5;
+int Speed = 200; 
+int SpeedB = 255; //Go the maximum ammount you can turn
+
+void Forward();
+void Reverse(); 
+void Left();
+void Right(); 
+void Brake();
+
+void setup()
+{
+  Serial.begin(9600);
+  // set all the motor control pins to outputs
+  pinMode(enA, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(enB, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+}
+
+void loop()
+{
+  // establish variables for duration of the ping, 
+  // and the distance result in inches and centimeters:
+  long duration, inches, cm;
+
+  // The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
+  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  pinMode(pingPin, OUTPUT);
+  digitalWrite(pingPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pingPin, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(pingPin, LOW);
+
+  // The same pin is used to read the signal from the PING))): a HIGH
+  // pulse whose duration is the time (in microseconds) from the sending
+  // of the ping to the reception of its echo off of an object.
+  pinMode(pingPin, INPUT);
+  duration = pulseIn(pingPin, HIGH);
+
+  // convert the time into a distance
+  inches = microsecondsToInches(duration);
+  cm = microsecondsToCentimeters(duration);
+
+  Serial.print(inches);
+  Serial.print("in, ");
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+
+  if (inches < 12){
+    Brake(); 
+    delay(2000);
+  }
+  else {
+    Forward();
+    delay(2000);
+  }
+  
+//  //Backup to the left 
+//  Left(); 
+//  delay(2000);
+//  Backward();
+//  delay(2000);
+//  Brake(); 
+//  delay(2000);
+//
+//  //Forward to the right
+//  Right(); 
+//  delay(2000);
+//  Forward(); 
+//  delay(2000);
+//  Brake(); 
+//  delay(2000);
+}
+//Ping Sensor 
+long microsecondsToInches(long microseconds) {
+  // According to Parallax's datasheet for the PING))), there are
+  // 73.746 microseconds per inch (i.e. sound travels at 1130 feet per
+  // second).  This gives the distance travelled by the ping, outbound
+  // and return, so we divide by 2 to get the distance of the obstacle.
+  // See: http://www.parallax.com/dl/docs/prod/acc/28015-PING-v1.3.pdf
+  return microseconds / 74 / 2;
+}
+
+long microsecondsToCentimeters(long microseconds) {
+  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
+  // The ping travels out and back, so to find the distance of the
+  // object we take half of the distance travelled.
+  return microseconds / 29 / 2;
+}
+
+//Car Directions 
+void Left(){
+  analogWrite(enB, SpeedB);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);  
+}
+void Right(){
+  analogWrite(enB, SpeedB);
+  digitalWrite(in4, LOW);  
+  digitalWrite(in3, HIGH);
+}
+void Forward()
+{
+  analogWrite(enA, Speed);
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+}
+void Backward()
+{
+  analogWrite(enA, Speed);
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+}
+void Brake()
+{
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+  analogWrite(enA, Speed);
+  analogWrite(enB, Speed);
+}
